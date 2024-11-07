531b728f9c2557fc8d0e8fe2ff508287a5108700
DenisS7 <220326@buas.nl>
2024-11-07 12:19:59 +0100

    Cleanup
    Cleaned up code a bit.

--- Source/WVC_Building/Public/BuildingPiece.h
+++ Source/WVC_Building/Public/BuildingPiece.h
@@ -22,41 +22,15 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UStaticMeshComponent* StaticMeshComponent;
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UProceduralMeshComponent* ProceduralMeshComponent;
 	UPROPERTY(EditAnywhere)
 	UMeshDeformerComponent* MeshDeformerComponent;
-	//TArray<FVector> CageControlPoints;
-	///** Original vertices of the target mesh */
-	//TArray<FVector> OriginalMeshVertices;
-//
-	//TArray<FVector> CageBaseVertices;
-	//float CageHeight;
-	///** MVC weights for each mesh vertex */
-	//TArray<float> MVCWeights;
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	void DeformMesh(const TArray<FVector>& CageBase, const float CageHeight);
-	
-	//void InitializeProceduralMesh();
-	//void UpdateDeformation(const TArray<FVector>& CageDeformedVertices);
-	///** Computes Mean Value Coordinates (MVC) weights for the mesh vertices */
-	//TArray<float> ComputeMVCWeights(const TArray<FVector>& MeshVertices, const TArray<FVector>& CageVertices);
-//
-	///** Extracts the vertices from the procedural mesh */
-	//TArray<FVector> GetMeshVertices();
-//
-	///** Apply the updated vertices to the procedural mesh */
-	//void ApplyVerticesToProceduralMesh(const TArray<FVector>& Vertices);
-//
-	///** Gets the updated cage vertices (you'll need to implement this as per your logic) */
-	//TArray<FVector> GetUpdatedCageVertices();
-	//
-	//void DeformMesh(const TArray<FVector>& CageBase, const float CageHeight);
-	//FVector ApplyLaticeDeformer(const FVector& VertexPosition);
-	//void CalculateNormals(const TArray<FVector>& InVertices, const TArray<int32>& InTriangles, TArray<FVector>& OutNormals);
 };
--- Source/WVC_Building/Public/MeshDeformerComponent.h
+++ Source/WVC_Building/Public/MeshDeformerComponent.h
@@ -21,59 +21,32 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
-	// Reference to the Procedural Mesh Component
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh Deformer")
 	UProceduralMeshComponent* ProceduralMeshComp = nullptr;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh Deformer")
 	UStaticMesh* StaticMeshComp = nullptr;
-	// Original and deformed vertices
+
 	TArray<FVector> OriginalVertices;
 	TArray<FVector> DeformedVertices;
-//
-	//// Triangles, normals, UVs, etc.
+
 	TArray<int32> Triangles;
 	TArray<FVector> Normals;
 	TArray<FVector2D> UVs;
 	TArray<FProcMeshTangent> Tangents;
-//
-	//// QMVC weights for each vertex
-	//TArray<TArray<float>> Weights;
-//
-	//// Initial and current cage vertices
-	///
+	
 	TArray<FVector> InitialCageVertices;
 	TArray<FVector> CageVertices;
 	TArray<TArray<int>> CageFaces;
-	//TArray<FVector> CageVerticesCurrent;
-//
-	//// Function to initialize mesh data
 	void InitializeMeshData(const TArray<FVector>& InVertices, const TArray<int32>& InTriangles,
 							const TArray<FVector>& InNormals, const TArray<FVector2D>& InUVs,
 							const TArray<FProcMeshTangent>& InTangents);
 
 	void DeformMesh(const TArray<FVector>& InCageVertices, const float InHeight);
-	// Function to initialize deformation
-	//UFUNCTION(BlueprintCallable, Category = "Mesh Deformer")
-	//void InitializeDeformation(const TArray<FVector>& CageVertices);
-
-	// Function to update deformation
-	//UFUNCTION(BlueprintCallable, Category = "Mesh Deformer")
-	//void UpdateDeformation(const TArray<FVector>& NewCageVertices);
 private:
 	FVector ComputeSMVCCoordinate(const FVector& OriginalCoordinate);
 	void ComputeSMVCWeights(const FVector& Point, TArray<double>& OutWeights);
 	double GetAngleBetweenUnitVectors(const FVector& U1, const FVector& U2);
 	double GetTangentOfHalfAngleBetweenUnitVectors(const FVector& U1, const FVector& U2);
-	// Compute QMVC weights
-	//void ComputeQMVCWeights();
-//
-	//// Update mesh vertices based on cage deformation
-	//// Compute the QMVC weight for a vertex
-//
-	//// Helper functions for QMVC
-	//FVector ComputeEdgeVector(const FVector& Vi, const FVector& Vj){return FVector::ZeroVector;};
-	//float ComputeEdgeWeight(const FVector& Pi, const FVector& Pj, const FVector& PiMinusPj);
-	//TArray<int32> GetAdjacentVertices(int32 VertexIndex);
-	// ... (Other necessary helper functions)
+
 };
