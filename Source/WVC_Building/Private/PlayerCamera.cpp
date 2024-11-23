a3519c8f3f323c73c426d443b9cb986a90537d20
DenisS7 <220326@buas.nl>
2024-11-23 02:00:10 +0100

    Building tiles can be removed
    Building pieces can be removed on right click.
    Moved code to update a quad mesh in UtilityLibrary.

--- Source/WVC_Building/Private/PlayerCamera.cpp
+++ Source/WVC_Building/Private/PlayerCamera.cpp
@@ -78,201 +78,40 @@ void APlayerCamera::OnLeftMouseButtonPressed()
 	{
 		return;
 	}
-	
-	//if(Grid->GetSecondGrid()[Shape].Points.Num() != 4)
-	//	return;
-	
+
 	const FGridShape& GridShape = Grid->GetSecondGrid()[Shape];
 	const TArray<int>& ShapePoints = GridShape.Points;
 	Grid->MarchingBits[0][GridShape.CorrespondingGrid1Point] = true;
 	for(int i = 0; i < ShapePoints.Num(); i++)
 	{
-		const FGridQuad& CorrespondingQuad = Grid->GetFinalQuads()[ShapePoints[i]];
-		bool Has0 = Grid->MarchingBits[0][CorrespondingQuad.Points[0]];
-		int MinCorner = -1;
-		int MaxMinCorner = -1;
-		TArray<FVector> CageBase;
-		for(int k = 0; k < 4; k++)
-			CageBase.Add(Grid->GetPointCoordinates(CorrespondingQuad.Points[k]));
-
-		//if()
-		auto Find = Grid->BuildingPieces.Find(TPair<int, int>(0, ShapePoints[i]));
-		ABuildingPiece* BuildingPiece;
-		if(!Find)
-		{
-			BuildingPiece = GetWorld()->SpawnActor<ABuildingPiece>(BuildingPieceToSpawn, CorrespondingQuad.Center, FRotator(0, 0, 0));
-			Grid->BuildingPieces.Add(TPair<int, int>(0, ShapePoints[i]), BuildingPiece);
-		}
-		else
-		{
-			BuildingPiece = *Find;
-			BuildingPiece->Corners.Empty();
-			//BuildingPiece->MarchingCorners
-		}
-		//Grid->FirstElevation[ShapePoints[i]].ConfigurationCorners[i] = true;
-		//Grid->VoxelConfig[0][ShapePoints[i]].ConfigurationCorners[i] = true;
-		int TileConfig = 0;
-		//int Corners = 0;
-		//float Direction = 0.f;
-		TArray<int> LowerCorners;
-		TArray<int> UpperCorners;
-		TArray<bool> CornersMask;
-		for(int j = 0; j < 8; j++)
-		{
-			//TileConfig += Grid->FirstElevation[ShapePoints[i]].ConfigurationCorners[j] * pow(10, 8 - j);
-			TileConfig += Grid->MarchingBits[j / 4][CorrespondingQuad.Points[j % 4]] * pow(10, 8 - j - 1);
-			CornersMask.Add(Grid->MarchingBits[j / 4][CorrespondingQuad.Points[j % 4]]);
-			//DELETE WHEN UPPER PART IS USED
-			if(Grid->MarchingBits[j / 4][CorrespondingQuad.Points[j % 4]])
-			{
-				if(j < 4)
-					LowerCorners.Add(j);
-				else
-					UpperCorners.Add(j);
-				//Corners.Add((j / 4) * 4 + j % 4);
-				if(MinCorner == -1)
-					MinCorner = j;
-				if(j < 4)
-					MaxMinCorner = j;
-			}
-			
-			float Height = 0.f;
-			if(j >= 4)
-				Height = 200.f;
-			//DrawDebugBox(GetWorld(), Grid->GetPointCoordinates(CorrespondingQuad.Points[FMath::Min(j, 3)]) + FVector(0.f, 0.f, Height), FVector(10.f), Grid->MarchingBits[j / 4][CorrespondingQuad.Points[j % 4]] ? FColor::Green : FColor::Red, false, 100.f);
-		}
-		
-		//if(Has0)
-		//{
-		//	while(Has0)
-		//	{
-		//		for(int j = 0; j < Corners.Num(); j++)
-		//		{
-		//			Corners[j]++;
-		//			Corners[j];
-		//		}
-		//	}
-		//}
-
-		float Rotation = 0.f;
-		
-		if(LowerCorners.Num())
-		{
-			if(LowerCorners.Num() >= 2)
-			{
-				for(int j = 0; j < LowerCorners.Num() - 1; j++)
-				{
-					if(LowerCorners[j] != LowerCorners[j + 1] - 1)
-					{
-						for(int p = 0; p < LowerCorners.Num() - j - 1; p++)
-						{
-							const int LastElement = LowerCorners.Last();
-							for(int k = LowerCorners.Num() - 1; k >= 1; k--)
-							{
-								LowerCorners[k] = LowerCorners[k - 1];
-							}
-							LowerCorners[0] = LastElement;
-						}
-						break;
-					}
-				}
-			}
-
-			if(LowerCorners[0] != 0)
-			{
-				int RotationAmount = 4 - LowerCorners[0];
-				Rotation = static_cast<float>(RotationAmount) * 90.f;
-				for(int j = 0; j < LowerCorners.Num(); j++)
-				{
-					LowerCorners[j] = (LowerCorners[j] + RotationAmount) % 4;
-				}
-			}
-			//if(MinCorner == 0 && CornersMask[3])
-			//{
-			//	while(MaxMinCorner != 0)
-			//	{
-			//		for(int j = 0; j < 8; j++)
-			//		{
-			//			Corners[j]++;
-			//			if(j < 4)
-			//				Corners[j] %= 4;
-			//			else
-			//			{
-			//				Corners[j] %= 4;
-			//				Corners[j] += 4;
-			//			}
-			//		}
-			//		//CageBase.Swap(0,3);
-			//		//CageBase.Swap(1,3);
-			//		//CageBase.Swap(2,3);
-			//		MaxMinCorner++;
-			//		MaxMinCorner %= 4;
-			//		Rotation += 90.f;
-			//	}
-			//}
-			//else
-			//{
-			//	while(MinCorner != 0)
-			//	{
-			//		for(int j = 0; j < 8; j++)
-			//		{
-			//			Corners[j]--;
-			//			if(j >= 4 && Corners[j] < 4)
-			//				Corners[j] += 4;
-			//		}
-			//		//CageBase.Swap(0,3);
-			//		//CageBase.Swap(0,2);
-			//		//CageBase.Swap(0,1);
-			//		MinCorner--;
-			//		Rotation -= 90.f;
-			//	}
-			//	for(int j = 0; j < 4; j++)
-			//	{
-			//		Corners[j] = FMath::Abs(Corners[j]);
-			//	}
-			//}
-		}
-		
-		FString RowName;
-		//TArray<int> UsedCorners;
-		//for(int j = 0; j < 8; j++)
-		//	if(CornersMask[j])
-		//		UsedCorners.Add(Corners[j]), BuildingPiece->Corners.Add(j);
-		//UsedCorners.Sort();
-		BuildingPiece->Corners = LowerCorners;
-		BuildingPiece->Corners.Append(UpperCorners);
-		for(int j = 0; j < LowerCorners.Num(); j++)
-			RowName.Append(FString::FromInt(LowerCorners[j]));
-		for(int j = 0; j < UpperCorners.Num(); j++)
-			RowName.Append(FString::FromInt(UpperCorners[j]));
-		
-		FName RowNameName(RowName);
-		//RowNameName = "01";
-		auto Row = BuildingPiece->DataTable->FindRow<FMeshCornersData>(RowNameName, "MeshCornersRow");
-		BuildingPiece->SetStaticMesh(Row->Mesh);
-		//UStaticMesh* Mesh2;
-		//BuildingPiece->AddActorWorldRotation(FRotator(0.f, Rotation, 0.f));
-		BuildingPiece->DeformMesh(CageBase, 200.f, Rotation);
+		UtilityLibrary::UpdateBuildingPiece(GetWorld(), Grid, ShapePoints[i], BuildingPieceToSpawn);
 	}
 	
 	//WVC Step to determine the pool
-	
-	
-	//TArray<FVector> CageBase;
-	//for(int i = 0; i < 4; i++)
-	//	CageBase.Add(Grid->GetSecondPointCoordinates(Grid->GetSecondGrid()[Shape].Points[i]));
-	//ABuildingPiece* NewBuildingPiece = GetWorld()->SpawnActor<ABuildingPiece>(BuildingPieceToSpawn, Grid->GetSecondGrid()[Shape].Center, FRotator(0, 0, 0));
-	//NewBuildingPiece->DeformMesh(CageBase, 200.f);
 }
 
 void APlayerCamera::OnLeftMouseButtonReleased()
 {
-	
+
 }
 
 void APlayerCamera::OnRightMouseButtonPressed()
 {
 	IsDragging = true;
+	AGridGenerator* Grid = nullptr;
+	int Shape = -1;
+	UtilityLibrary::GetGridAndShapeMouseIsHoveringOver(GetWorld(), Grid, Shape);
+	if(!Grid || Shape < 0)
+	{
+		return;
+	}
+	const FGridShape& GridShape = Grid->GetSecondGrid()[Shape];
+	const TArray<int>& ShapePoints = GridShape.Points;
+	Grid->MarchingBits[0][GridShape.CorrespondingGrid1Point] = false;
+	for(int i = 0; i < ShapePoints.Num(); i++)
+	{
+		UtilityLibrary::UpdateBuildingPiece(GetWorld(), Grid, ShapePoints[i], BuildingPieceToSpawn);
+	}
 }
 
 void APlayerCamera::OnRightMouseButtonReleased()
